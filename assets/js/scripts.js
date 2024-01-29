bb0070e5d81d29a5c56af8d2d3f61cd45f6d6f70
Kevinwmiguel <kevincursos@outlook.com>
2024-01-29 03:51:50 +0000

    Funções e style

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,58 @@
-function contas(id){
-    var inside = document.getElementById(id).innerText;
-    console.log(inside);
+function contas(value) {
+    var result = document.querySelector('.result');
+    result.innerText += value;
 }
+
 function answer() {
-    var results = document.getElementsByClassName('result');
-    var expression = ""; // Construa aqui a expressão a partir dos elementos clicados
-
-    // Lógica para calcular a expressão (dependendo da sua implementação)
-
-    // Exiba o resultado
-    for (var i = 0; i < results.length; i++) {
-        results[i].innerText = resultadoCalculado;
+    var result = document.querySelector('.result');
+    try {
+        result.innerText = eval(result.innerText);
+    } catch (error) {
+        result.innerText = 'Erro';
     }
 }
 
-function clear(){
-    var results = document.getElementsByClassName('result')
+function clearResult() {
+    var result = document.querySelector('.result');
+    result.innerText = '';
+}
 
-    for (var i = 0; i < results.length; i++) {
-        results[i].innerText = "";
+var keyPressed = {};
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        answer();
+    } else if (event.key === " ") {
+        clearResult();
+    } else if (event.key === "Backspace") {
+        handleBackspace();
+    }
+    else if ((event.key < 'a' || event.key > 'z')  && !keyPressed[event.key]) {
+        if (event.key < 'A' || event.key > 'Z'){
+            contas(event.key);
+            keyPressed[event.key] = true;
+        }   
+    }
+    
+});
+
+function handleBackspace() {
+    // Adicione sua lógica para tratar a tecla "Backspace" aqui
+    // Por exemplo, você pode remover o último caractere da expressão no resultado
+    var result = document.querySelector('.result');
+    var currentExpression = result.innerText;
+    
+    if (currentExpression.length > 0) {
+      result.innerText = currentExpression.slice(0, -1);
     }
 }
+
+document.addEventListener("keyup", function(event) {
+    if (event.key) {
+        keyPressed[event.key] = false;
+    }
+});
+
+
+
+
